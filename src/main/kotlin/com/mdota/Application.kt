6e9846b90a81635ac61b01a984a370131505aa96
D1maD1m0nd <d1m0hkrasav4ik@gmail.com>
2022-10-25 03:08:09 +0500

    initial commit

--- src/main/kotlin/com/mdota/Application.kt
+++ src/main/kotlin/com/mdota/Application.kt
@@ -5,9 +5,6 @@ import io.ktor.server.netty.*
 import com.mdota.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
-        configureHTTP()
-        configureSerialization()
-        configureRouting()
-    }.start(wait = true)
+    val client = HttpClient(CIO)
+
 }
